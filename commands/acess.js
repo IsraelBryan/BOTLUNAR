e9792fc572edcbfb20499b1cc3f80d9f6390ea48
UMBFB1 <umbreongame0@gmail.com>
2024-07-02 10:52:35 -0300

    02/07/2024

--- commands/acess.js
+++ commands/acess.js
@@ -1,4 +1,4 @@
-const {SlashCommandBuilder, EmbedBuilder} = require('discord.js');
+const {SlashCommandBuilder, EmbedBuilder, MessageEmbed} = require('discord.js');
 
 
 const accessEmbed = new EmbedBuilder()
@@ -17,8 +17,44 @@ module.exports = {
     .setDescription("custom collars to your avatar!"),
 
     async execute(interaction) {
-        await interaction.reply({embeds: [accessEmbed]})
-		await InitialMessage.reply('⬅️')
-		await  InitialMessage.reply('⬅️')
+		const accessEmbed = new EmbedBuilder()
+		.setColor(0x0099FF)
+		.setTitle('Collar-Wicker')
+		.setURL('https://discord.js.org/')
+		.setDescription('Some description here')
+		.setThumbnail('https://i.imgur.com/AfFp7pu.png')
+		.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+		.setImage('https://i.imgur.com/AfFp7pu.png')
+		.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+
+			//Embed inicial
+			const InitialEmbed = await interaction.reply({embeds : [accessEmbed]})
+
+			//Adicionando reações
+			await InitialEmbed.react('⬅️');
+			await InitialEmbed.react('➡️');
+
+			//Filtro das reações
+			const filter = (reaction, user) => {
+				return ['⬅️', '➡️'].includes(reaction.emoji.name) && user.id === interaction.user.id;
+			}
+
+			//Coletor
+			const collector =InitialEmbed.createReactionCollector({ filter, time: 60000});
+
+			collector.on('collect', async (reaction) => {
+				if(reaction.emoji.name === '⬅️'){
+					accessEmbed.setDescription('Old');
+					await InitialEmbed.edit({embeds : [accessEmbed] });
+
+				} else if(reaction.emoji.name === '➡️'){
+					accessEmbed.setDescription('New');
+					await InitialEmbed.edit({embeds : [accessEmbed] });
+				}
+			});
+
+			collector.on('end', collected => {
+				console.log(`Foram coletados ${collected.size} reações.`)
+			});
     }
 }
