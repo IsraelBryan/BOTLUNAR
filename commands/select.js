ce1c5e74b90b4053be5c45274380167acd55d058
UMBFB1 <umbreongame0@gmail.com>
2024-07-12 19:58:33 -0300

    12/07/2024

---
+++ commands/select.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder, ActionRowBuilder, StringSelectMenuBuilder } = require('discord.js');
+
+const select = new ActionRowBuilder()
+    .addComponents(
+        new StringSelectMenuBuilder()
+            .setCustomId("select")
+            .setPlaceholder("Nenhum tópico selecionado")
+            .addOptions({
+                label: "nardoragon",
+                description: "Veja todas as categorias associadas para Nardoragon.",
+                value: "nardoragon"
+
+            })
+    )
+
+    module.exports = {
+        data: new SlashCommandBuilder()
+        .setName("select")
+        .setDescription("selecione categorias!"),
+    
+        async execute(interaction) {
+            await interaction.reply({contents: "Selecione uma dessas categorias: ", components: [select]})
+        }
+    }
