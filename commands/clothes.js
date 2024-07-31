e02a764d11ad53f0a6da49367eef6d2b3b10c645
UMBFB1 <umbreongame0@gmail.com>
2024-07-31 18:18:43 -0300

    31/07/2024

--- commands/clothes.js
+++ commands/clothes.js
@@ -8,7 +8,7 @@ const accessEmbed = new EmbedBuilder()
 	.setDescription('Clothes for different type of avatars!')
 	.setThumbnail('https://booth.pximg.net/e6deeb51-72a5-4562-9425-0bdd110b534d/i/5832850/77ee8746-eadb-4a3d-937b-73c238f1fe9d_base_resized.jpg')
 	.addFields({ name: 'Web name:', value: 'VRChive', inline: true })
-	.setImage('https://bauwhmtbztaunmwfgaqi.supabase.co/storage/v1/object/public/asset-thumbnails/1634317926479/8e257237-829e-42ce-a857-32ffd8b9f087.webp')
+	.setImage('https://booth.pximg.net/1832527b-fd7e-4ad1-b31d-b883f1c6bcfb/i/5393738/48939daa-4f4f-4486-bc2e-79ee0772ce09_base_resized.jpg')
 
 module.exports = {
 	data: new SlashCommandBuilder()
