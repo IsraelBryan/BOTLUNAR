ddc2a4b74718bc31e25c9ef8f651e3c1c1aff97f
UMBFB1 <umbreongame0@gmail.com>
2024-07-31 18:36:43 -0300

    31/07/2024

--- commands/select.js
+++ commands/select.js
@@ -20,6 +20,31 @@ const select = new ActionRowBuilder()
                 description:"Deira model download",
                 value: "deira"
             })
+            .addOptions({
+                label: "rindo",
+                description: "Rindo model download",
+                value: "rindo"
+            })
+            .addOptions({
+                label: "karin",
+                description: "Karin model download",
+                value: "karin"
+            })
+            .addOptions({
+                label: "kikyo",
+                description:"Kikyo model download",
+                value: "kikyo"
+            })
+            .addOptions({
+                label: "chibi_novabeast",
+                description: "Chibi Novabeast model download",
+                value: "chibi_novabeast"
+            })
+            .addOptions({
+                value: "mamehinata",
+                description: "Mamehinata V1.53 model download",
+                value: "mamehinata"
+            })
     )
 
 module.exports = {
