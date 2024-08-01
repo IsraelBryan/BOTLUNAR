60064eff00ebe08f5a884aadcbe88ba75b5393a9
UMBFB1 <umbreongame0@gmail.com>
2024-08-01 07:54:14 -0300

    01/08/2024

--- commands/models-gt.js
+++ commands/models-gt.js
@@ -2,20 +2,20 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 const modelsEmbed = new EmbedBuilder()
     .setColor(0xea20db)
-    .setTitle('Free Models!')
+    .setTitle('Unity')
     .addFields(
-        { name: 'Nardoragon [CUIDADO, SITE 18+]', value: 'https://c4.kemono.su/data/de/9a/de9accc75ba099078e73d6214f081e86c95388bd319b13c63376ce757671b0b0.gz?f=5%20Fingers%20PC%20V2.1.unitypackage' },
+        { name: 'Unity Hub', value: 'https://unity.com/pt/download' },
         { name: '\u200B', value: '\u200B' },
-        { name: 'Wickerbeast', value: 'https://vrmodels.store/avatars/39341-kurais-female-wickerbeast-avatar.html', inline: true },
+        { name: 'Unity 2022.3.22f1', value: 'https://unity.com/pt/releases/editor/whats-new/2022.3.22#installs', inline: true },
         { name: '\u200B', value: '\u200B' },
-        { name: 'Deira', value: 'https://vrmodels.store/avatars/34865-star-pup-deira-edit.html' },
+        { name: 'Unity 2019.4.31', value: 'https://unity.com/pt/releases/editor/whats-new/2019.4.31#installs' },
     )
 
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName("models-gt")
-        .setDescription("modelos de avatares do vrchat gratuitos! [VRModels]"),
+        .setName("unity")
+        .setDescription("Unity downloads!"),
 
     async execute(interaction) {
         await interaction.reply({ embeds: [modelsEmbed] })
