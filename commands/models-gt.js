e771d4d42b4b2a9223b751552685e18fc2cff147
UMBFB1 <umbreongame0@gmail.com>
2024-08-01 07:56:12 -0300

    01/08/2024

--- commands/models-gt.js
+++ commands/models-gt.js
@@ -3,6 +3,8 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const modelsEmbed = new EmbedBuilder()
     .setColor(0xea20db)
     .setTitle('Unity')
+    .setThumbnail('https://cdn.domestika.org/c_fill,dpr_auto,f_auto,q_auto,w_1200/v1635855213/blog-post-open-graph-covers/000/005/309/5309-original.jpg?1635855213')
+    .setImage('https://upload.wikimedia.org/wikipedia/commons/c/c4/Unity_2021.svg')
     .addFields(
         { name: 'Unity Hub', value: 'https://unity.com/pt/download' },
         { name: '\u200B', value: '\u200B' },
