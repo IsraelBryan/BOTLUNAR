ff967d21f724fd3629ec52e95d2c8195a41c1e77
UMBFB1 <umbreongame0@gmail.com>
2024-08-01 14:19:33 -0300

    01/08/2024

--- commands/ping.js
+++ commands/ping.js
@@ -1,12 +1,27 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+const commandsEmbed = new EmbedBuilder()
+	.setColor(0xea00db)
+	.setTitle('Comandos!')
+	.addFields(
+		{ name: 'Clothes', value: 'Link que leva a uma lista de roupas para diversos tipos de avatares!' },
+		{ name: '\u200B', value: '\u200B' },
+		{ name: 'Select', value: 'Lhe disponibiliza escolhas para diversos tipos de downloads de avatares! Lembrando que terá que fazer uma conta no devido site para baixar.', inline: true },
+		{ name: '\u200B', value: '\u200B' },
+		{ name: 'Unity', value: 'Lhe oferece os arquivos para baixar a unity!' },
+		{ name: '\u200B', value: '\u200B' },
+		{ name: 'Playlist-oficial', value: 'Ouça as músicas recomendadas pelo bot em sua playlist oficial!' },
+		{ name: '\u200B', value: '\u200B' },
+		{ name: 'Atualização', value: 'Lhe dar informações atualizadas sobre respectivos sites destinados nas listas!' },
+	)
 
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName("ping")
-        .setDescription("Responde com Pong!"),
+        .setName("commands")
+        .setDescription("Lista de comandos para lhe manter informado!"),
 
     async execute(interaction) {
-        await interaction.reply("Pong!")
+        await interaction.reply({embeds: [commandsEmbed]})
     }
 }
