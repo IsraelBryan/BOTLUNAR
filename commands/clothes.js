eac9aa5e1a268b68d505f2a728bcc1671cd7b52f
UMBFB1 <umbreongame0@gmail.com>
2024-08-03 19:59:42 -0300

    03/08/2024

--- commands/clothes.js
+++ commands/clothes.js
@@ -9,6 +9,7 @@ const accessEmbed = new EmbedBuilder()
 	.setThumbnail('https://booth.pximg.net/e6deeb51-72a5-4562-9425-0bdd110b534d/i/5832850/77ee8746-eadb-4a3d-937b-73c238f1fe9d_base_resized.jpg')
 	.addFields({ name: 'Web name:', value: 'VRChive', inline: true })
 	.setImage('https://booth.pximg.net/1832527b-fd7e-4ad1-b31d-b883f1c6bcfb/i/5393738/48939daa-4f4f-4486-bc2e-79ee0772ce09_base_resized.jpg')
+	.setFooter('Website: VRChive')
 
 module.exports = {
 	data: new SlashCommandBuilder()
