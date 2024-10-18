8694dd26fe6c445a2bfcd6c1ce67ec1c71ad45d6
UMBFB1 <umbreongame0@gmail.com>
2024-10-18 10:14:24 -0300

    18/10/2024

--- commands/prisma.js
+++ commands/prisma.js
@@ -1,20 +1,21 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const {SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { execute } = require('./clothes');
 
 const prisma = new EmbedBuilder()
-    .setColor(0x0046FF)
-    .setTitle("Prismic's Search")
-    .setURL('https://vrcpirate.com/vrcpirate/archive/pas/index.html')
-    .setDescription("Prismic's avatar search now WEB!")
-    .setThumbnail('https://vrcpirate.com/vrcpirate/archive/pas/index.html')
-    .addFields({ name: 'Link', value: 'https://vrcpirate.com/vrcpirate/archive/pas/index.html'})
-    .setImage('https://vrcpirate.com/vrcpirate/archive/pas/index.html')
+.setTitle("Prismic!")
+.setDescription("Prismic's search avatar now WEB!!")
+.setThumbnail("https://vrcpirate.com/vrcpirate/archive/pas/index.html")
+.setColor(0x0046FF)
+.setImage("https://vrcpirate.com/vrcpirate/archive/pas/index.html")
+.addFields({ name: "Link", value: "https://vrcpirate.com/vrcpirate/archive/pas/index.html"})
+
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName("prisma")
-        .setDescription("Prismic's avatar search now WEB!"),
+    .setName('prismic')
+    .setDescription("Prismic's avatar search now WEB!!"),
 
     async execute(interaction) {
-        await interaction.reply({ embeds: [prisma]})
+        await interaction.reply({ embed: [prisma] })
     }
 }
